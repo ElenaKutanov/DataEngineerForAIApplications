2f0550e9f404b398a6ba2635f6768542ea06405e
Elena Kutanov <elena.kutanov@ese.de>
2022-11-09 13:42:23 +0100

    DB successfully separated!

--- UdaConnect/scripts/run_full_deployment.sh
+++ UdaConnect/scripts/run_full_deployment.sh
@@ -3,6 +3,13 @@ sh scripts/run_persons_deployment.sh
 sh scripts/run_frontend_deployment.sh
 sh scripts/run_locations_deployment.sh
 
+# Erase db k8s services and deployments
+kubectl delete deployment postgres-persons
+kubectl delete services postgres-persons
+
+kubectl delete deployment postgres-locations
+kubectl delete services postgres-locations
+
 # Apply deployments
 kubectl apply -f deployment/
 
